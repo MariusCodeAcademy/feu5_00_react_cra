64b1e017ad14c1efa9cab4d51e12a43391d1e5d4
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-20 11:35:37 +0200

    hero and 3 sections

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,32 @@ function App() {
       <div className="hero">
         <img src="https://picsum.photos/id/8/1200/800" alt="hero" />
       </div>
-      <h1 id="hi">Hello {5 + 5}</h1>
+
+      <div className="gridThree">
+        <article className="sectionPart">
+          <h3>About</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            doloremqem magni, culpa eveniet unde animi saepe id aliquid
+            voluptatibus minima dolores! Ab molestiae velit deleniti
+          </p>
+        </article>
+        <article className="sectionPart">
+          <h3>Company</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            doloremque officiis voluptatem magni, culpa eveniet unde animi saepe
+          </p>
+        </article>
+        <article className="sectionPart">
+          <h3>Services</h3>
+          <p>
+            Lorem ipsum consectetur adipisicing elit. Dignissimos doloremque
+            officiis voluptatem magni, culpa eveniet unde animi saepe id aliquid
+            voluptatibus minima dolores! Ab molestiae velit deleniti
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
