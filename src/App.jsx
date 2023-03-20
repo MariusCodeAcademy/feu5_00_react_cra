abe15eded70fc71092d7e8ac91dc7fcd2a95e26a
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-20 12:31:17 +0200

    three list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/layout/Header';
 import Hero from './components/Hero';
+import ThreeList from './components/three/ThreeList';
 import './App.css';
 
 function App() {
@@ -7,31 +8,7 @@ function App() {
     <div className="container">
       <Header />
       <Hero />
-      <div className="gridThree">
-        <article className="sectionPart">
-          <h3>About</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            doloremqem magni, culpa eveniet unde animi saepe id aliquid
-            voluptatibus minima dolores! Ab molestiae velit deleniti
-          </p>
-        </article>
-        <article className="sectionPart">
-          <h3>Company</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            doloremque officiis voluptatem magni, culpa eveniet unde animi saepe
-          </p>
-        </article>
-        <article className="sectionPart">
-          <h3>Services</h3>
-          <p>
-            Lorem ipsum consectetur adipisicing elit. Dignissimos doloremque
-            officiis voluptatem magni, culpa eveniet unde animi saepe id aliquid
-            voluptatibus minima dolores! Ab molestiae velit deleniti
-          </p>
-        </article>
-      </div>
+      <ThreeList />
     </div>
   );
 }
