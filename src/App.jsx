726743be686bbb83fd216ba3a6504fefefbcc5a5
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-20 11:00:42 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 id='hi'>Hello {5 + 5}</h1>
+    <div className="container">
+      <header>
+        <h2 className="logo">
+          Logo<span>React</span>
+        </h2>
+        <nav className="mainNav">
+          <a href="#home" className="navLink">
+            Home
+          </a>
+          <a href="#product" className="navLink">
+            Product
+          </a>
+          <a href="#company" className="navLink">
+            Company
+          </a>
+          <a href="#contact" className="navLink">
+            Contact
+          </a>
+        </nav>
+      </header>
+
+      <div className="hero">
+        <img src="https://picsum.photos/id/8/1200/800" alt="hero" />
+      </div>
+      <h1 id="hi">Hello {5 + 5}</h1>
     </div>
   );
 }
