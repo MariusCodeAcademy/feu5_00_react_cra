f62709a7fdc24af4e86813f65c11a156c8442a61
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-20 11:48:06 +0200

    header exported to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
+import Header from './components/layout/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <header>
-        <h2 className="logo">
-          Logo<span>React</span>
-        </h2>
-        <nav className="mainNav">
-          <a href="#home" className="navLink">
-            Home
-          </a>
-          <a href="#product" className="navLink">
-            Product
-          </a>
-          <a href="#company" className="navLink">
-            Company
-          </a>
-          <a href="#contact" className="navLink">
-            Contact
-          </a>
-        </nav>
-      </header>
+      <Header />
 
       <div className="hero">
         <img src="https://picsum.photos/id/8/1200/800" alt="hero" />
